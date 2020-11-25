04a8c29d7d53fb358a90b6af8ecdccbb5125668c
dare@darey.io <dare@darey.io>
2020-11-25 17:06:24 +0000

    remove debug

--- html/functions.php
+++ html/functions.php
@@ -6,13 +6,13 @@ $db = mysqli_connect('mysql.tooling.svc.cluster.local', 'admin', 'admin', 'tooli
 
 
 // Check connection
-if (mysqli_connect_errno()) {
-echo "Failed to connect to MySQL: " . mysqli_connect_error();
-exit();
-}
-else{
-echo "connected";
-}
+// if (mysqli_connect_errno()) {
+// echo "Failed to connect to MySQL: " . mysqli_connect_error();
+// exit();
+// }
+// else{
+// echo "connected";
+// }
 
 // variable declaration
 $username = "";
