6a618693cda3c00275b18f43594a4469ada1c8a5
Cloud User <ec2-user@ip-172-31-22-237.us-east-2.compute.internal>
2021-03-03 10:31:19 +0000

    updated functions.php

--- html/functions.php
+++ html/functions.php
@@ -2,7 +2,7 @@
 session_start();
 
 // connect to database
-$db = mysqli_connect('mysql.tooling.svc.cluster.local', 'admin', 'admin', 'tooling');
+$db = mysqli_connect('3.142.47.71', 'webaccess', 'password', 'toolingdb');
 
 // Check connection
 // if (mysqli_connect_errno()) {
